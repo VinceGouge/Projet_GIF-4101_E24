b6a5d6ac65a3624478a7fda47a9524b9bf1bb7c0
VinceGouge <vincent.gouge@gmail.com>
2021-11-26 23:04:59 -0500

    Correction du programme de filtrage, création de la matrice de feature et début du preprocessing

--- dataset_filtering.py
+++ dataset_filtering.py
@@ -5,21 +5,21 @@ browsing_file = pd.read_csv('../Dataset_RAW/train/browsing_train.csv')
 search_file = pd.read_csv('../Dataset_RAW/train/search_train.csv')
 sku_file = pd.read_csv('../Dataset_RAW/train/sku_to_content.csv')
 
-session_id_with_AC = browsing_file.query("product_action == 'add'")["session_id_hash"]          # session ayant un AC
-added_product = browsing_file.query("product_action == 'add'")["product_sku_hash"]                # produit ayant été AC
-browsingFiltered = pd.merge(browsing_file, session_id_with_AC, how='inner', on='session_id_hash')     # filtrage de browsing pour session avec AC
-searchFiltered = pd.merge(search_file, session_id_with_AC, how='inner', on='session_id_hash')         # filtrage de search pour session avec AC
-skuFiltered = pd.merge(sku_file, added_product, how="inner", on="product_sku_hash")                   # fitrage de sku pour produit AC
+sessions_id_with_AC = browsing_file.query("product_action == 'add'")["session_id_hash"].unique().tolist()   # liste des sessions ayant un AC
+added_product = browsing_file.query("product_action == 'add'")["product_sku_hash"].unique().tolist()    # liste des produits ayant été AC
+browsingFiltered = browsing_file.query("session_id_hash == @sessions_id_with_AC")   # filtrage de browsing pour session avec AC
+searchFiltered = search_file.query("session_id_hash == @sessions_id_with_AC")   # filtrage de search pour garder uniquement session avec AC
+sku_filtered = sku_file.query("product_sku_hash == @added_product") # filtrage de sku pour garder uniqument produit ayant été AC
 
 # création des fichiers csv filtrés
 browsingFiltered.to_csv(r'../Dataset_FILTERED/browsing_filtered.csv', header=True, index=False)
 searchFiltered.to_csv(r'../Dataset_FILTERED/search_filtered.csv', header=True, index=False)
-skuFiltered.to_csv(r'../Dataset_FILTERED/sku_filtered.csv', header=True, index=False)
+sku_filtered.to_csv(r'../Dataset_FILTERED/sku_filtered.csv', header=True, index=False)
 
-# 1000 premières lignes des fichiers filtré
-subset_browsingFiltered = browsingFiltered.head(1000)
+# 100000 premières lignes des fichiers filtré
+subset_browsingFiltered = browsingFiltered.head(100000)
 subset_browsingFiltered.to_csv(r'../Dataset_FILTERED/sub_browsing_filtered.csv', header=True, index=False)
-subset_searchFiltered = searchFiltered.head(1000)
+subset_searchFiltered = searchFiltered.head(100000)
 subset_searchFiltered.to_csv(r'../Dataset_FILTERED/sub_search_filtered.csv', header=True, index=False)
-subset_skuFiltered = skuFiltered.head(1000)
+subset_skuFiltered = sku_filtered.head(100000)
 subset_skuFiltered.to_csv(r'../Dataset_FILTERED/sub_sku_filtered.csv', header=True, index=False)
--- preprocessing.py
+++ preprocessing.py
@@ -7,25 +7,65 @@ root = '../Dataset_FILTERED/'
 
 # Lecture des fichiers csv en tableau pandas
 #browsing_file = pd.read_csv(root + 'browsing_filtered.csv')
-browsing_file = pd.read_csv(root + 'sub_browsing_filtered.csv')
 #search_file = pd.read_csv(root + 'search_filtered.csv')
-#sku_file = pd.read_csv(root + 'sku_filtered.csv')
+sku_file = pd.read_csv(root + 'sku_filtered.csv')
+browsing_file = pd.read_csv(root + 'sub_browsing_filtered.csv')
+search_file = pd.read_csv(root + 'sub_search_filtered.csv')
+#sku_file = pd.read_csv(root + 'sub_sku_filtered.csv')
 
-linesWithAdd = np.where(browsing_file['product_action'] == 'add')[0]    # index de toutes les intéractions AC
-linesWithPurchase = np.where(browsing_file['product_action'] == 'purchase')[0]    # index de tous les purchase
+"""
+Création d'un dataframe contenant les différents features nécessaires
+      session_id_hash (peut-être pertinant de remplacer par un id non-encrypté)
+      product_sku_hash (peut-être pertinant de remplacer par un id non-encrypté)
+      is_purchased
+      add_count_during_session
+      add_has_been_detailed
+      session_length
+      add_price
+"""
 
-# Création d'un dataframe contenant les différents features nécessaires
-#       session_id_hash (peut-être pertinant de remplacer par un id non-encrypté)
-#       product_sku_hash (peut-être pertinant de remplacer par un id non-encrypté)
-#       add_count_during_session
-#       add_has_been_detailed
-#       session_length
-#       add_price
-
-featureDataframe = browsing_file.loc[linesWithAdd]
+# Dataframe ou une ligne = un AC
+featureDataframe = browsing_file.query("product_action == 'add'")
 featureDataframe = featureDataframe.drop(['event_type', 'product_action', 'server_timestamp_epoch_ms', 'hashed_url'], axis=1)
-featureDataframe['add_count_during_session']=featureDataframe.groupby(by='session_id_hash')['session_id_hash'].transform('count')
+reindexedDataframe = featureDataframe.reset_index()
+
+is_purchased = np.zeros(len(featureDataframe))
+has_been_detailed = np.zeros(len(featureDataframe))
+
+sessions = featureDataframe['session_id_hash'].unique() # Liste des sessions distinctes ayant un AC
+for session in sessions:
+   session_AC = reindexedDataframe.query("session_id_hash == @session")
+   N = len(session_AC)
+   index = session_AC.index
+
+   purchased_product = browsing_file.query("session_id_hash == @session & product_action == 'purchase'")["product_sku_hash"]
+   if not purchased_product.empty:
+       purchased_product = purchased_product.tolist()
+       for prod in purchased_product:
+           index_purchased = index[session_AC["product_sku_hash"] == prod]
+           is_purchased[index_purchased] = 1
+
+   detailed_product = browsing_file.query("session_id_hash == @session & product_action == 'detail'")["product_sku_hash"]
+   if not detailed_product.empty:
+       detailed_product = detailed_product.tolist()
+       for detail in detailed_product:
+           index_detailed = index[session_AC["product_sku_hash"] == detail]
+           has_been_detailed[index_detailed] = 1
 
 
-print(featureDataframe)
+# Ajout du feature indiquant si le produit a été acheté
+featureDataframe['is_purchased'] = is_purchased
 
+# Ajout du feature indiquant le nombre de AC dans la session
+featureDataframe['add_count_during_session'] = featureDataframe.groupby(by='session_id_hash')['session_id_hash'].transform('count')
+
+# Ajout du feature indiquant si le produit a été détaillé
+featureDataframe['has_been_detailed'] = has_been_detailed
+
+# Ajout du feature indiquant le prix du produit
+added_product_price = pd.merge(featureDataframe, sku_file, on='product_sku_hash')['price_bucket'].tolist()
+#featureDataframe = featureDataframe.reset_index()
+featureDataframe['price'] = added_product_price
+
+
+featureDataframe.to_csv('features.csv', header=True, index=False)
